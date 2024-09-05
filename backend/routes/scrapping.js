95ffdb864729470502d5b17336e47b5090886e74
proyectos-developer <proyectos@developer-ideas.com>
2024-09-05 10:40:03 -0500

    V1.0.0

--- backend/routes/scrapping.js
+++ backend/routes/scrapping.js
@@ -5,30 +5,61 @@ const {Builder, By} = require ('selenium-webdriver')
 require ('selenium-webdriver/chrome')
 require ('chromedriver')
 
-
 const pool = require('../database')
-const { isLoggedIn } = require('../lib/auth')
 
 router.get ('/api/scrapping/:nro_ruc', async (req, res) => {
-    const {nro_ruc} = req.body
-
+    const {nro_ruc} = req.params
+    let ruc, nro_telefono, correo, nombre
+    let count = 0
     try {
         const driver = await new Builder().forBrowser('chrome').build()
         await driver.get(`https://apps.osce.gob.pe/perfilprov-ui/ficha/${nro_ruc}`)
 
-        const elements = await driver.findElements(By.className('profile-content'));
-        
-        for (const element of elements) {
-            const ruc = await element.findElement(By.tagName('info-value')).getText()
-            const nro_telefono = await element.findElement(By.tagName('info-value')).getText()
-            const correo = await element.findElement(By.tagName('emails')).getText()
-            return res.json ({
-                nro_ruc: ruc,
-                nro_telefono: nro_telefono,
-                correo: correo
-            })
+        const elements_cero = await driver.findElements(By.className('ficha__content'));
+        for (const element of elements_cero) {
+            if (count === 0){
+                nombre = await element.findElement(By.css('div.page__title')) ? 
+                      await element.findElement(By.css('div.page__title')).getText() : '-'
+                count = 1
+                break;
+            }
         }
+
+        const elements_uno = await driver.findElements(By.className('info'));
+        for (const element of elements_uno) {
+            if (count === 1){
+                ruc = await element.findElement(By.css('span.info-value')) ? 
+                      await element.findElement(By.css('span.info-value')).getText() : '-'
+                count = 2
+            }else if (count === 2){
+                nro_telefono = await element.findElement(By.css('span.info-value')) ? 
+                      await element.findElement(By.css('span.info-value')).getText() : '-'
+                count = 3
+                break;
+            }
+        }
+        
+        const elements_dos = await driver.findElements(By.className('emails-list'));
+        for (const element of elements_dos) {
+            if (count === 3){
+                correo = await element.findElement(By.css('span > a')) ? 
+                         await element.findElement(By.css('span > a')).getAttribute('href') : '-'
+                count = 4
+                break;
+            }
+        }
+        await driver.quit()
+        ruc = ruc.toString().split(':'[1])
+        const newData  = {nombre, correo, nro_telefono, ruc}
+        const new_data = await pool.query ('INSERT INTO data_empresa set ?', [newData])
+        const data = await pool.query ('SELECT * FROM data_empresa WHERE id = ?', [new_data.insertId])
+
+        return res.json ({
+            data_empresa: data,
+            success: true
+        })
     } catch (error) {
+        console.log (error)
         return res.json ({
             error: error,
             success: false
